b3308ee90cd99bf794f5f67f0dc4f2205f9ef34e
Dániel Morvai <daniel.morvai@atolldev.hu>
2021-04-13 12:36:38 +0200

    Extract config infos into default properties #4

--- paintball-admin-server/src/main/kotlin/server/admin/paintball/config/AppConfig.kt
+++ paintball-admin-server/src/main/kotlin/server/admin/paintball/config/AppConfig.kt
@@ -1,9 +1,11 @@
 package server.admin.paintball.config
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
 import org.springframework.stereotype.Component
 
 @Component
+@PropertySource(value = ["classpath:default.properties"])
 class AppConfig {
 
     @Value("\${map.images.location}")
